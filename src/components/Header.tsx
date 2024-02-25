75be628e7293a7860dc8d670ce90662ce7615359
allancruvinel <allan.cruvinel@gmail.com>
2024-02-25 15:21:54 -0300

    alterado alt

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import styles from './Header.module.css'
 export function Header(){
     return (
         <div className={styles.header}>
-            <img className={styles.img} src={LogoSvg} alt="" />
+            <img className={styles.img} src={LogoSvg} alt="teste" />
         </div>
     );
 }
